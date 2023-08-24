a6888a520dab3225baa8f8ec65becf5a33dc6081
Jack2153 <55502158+Jack2153@users.noreply.github.com>
2023-08-24 17:47:47 +0200

    Update startupSimSITL.py

--- startupSimSITL.py
+++ startupSimSITL.py
@@ -2,19 +2,40 @@
 import subprocess
 import time
 
- #Using the system.Popen() method to execute shell commands
+#Using the system.Popen() method to execute shell commands
 
 print("Starting the Simulator...")
-subprocess.Popen("gz sim -v 4 -r iris_empty.sdf", shell=True)
+
+My_Cmmnd1 = "gz sim -v 4 -r iris_empty.sdf"
+
+process2 = subprocess.Popen(
+    "gnome-terminal -e 'bash -c \""+My_Cmmnd1+";bash\"'",
+    stdout=subprocess.PIPE,
+    stderr=None,
+    shell=True
+)
+
 #time.sleep() function delays the programm in seconds
 time.sleep(3)
+
 print("Starting the SITL...")
 print("This may take a few moments")
 
-My_Cmmnd = "sim_vehicle.py -v ArduCopter -f gazebo-iris --model JSON --map --console"
+My_Cmmnd2 = "sim_vehicle.py -v ArduCopter -f gazebo-iris --model JSON --map --console"
 
-process = subprocess.Popen(
-    "gnome-terminal -e 'bash -c \""+My_Cmmnd+";bash\"'",
+process2 = subprocess.Popen(
+    "gnome-terminal -e 'bash -c \""+My_Cmmnd2+";bash\"'",
+    stdout=subprocess.PIPE,
+    stderr=None,
+    shell=True
+)
+
+print("Starting the ROS_gz bridge)
+
+My_Cmmnd3 = "ros2 run ros_gz_bridge parameter_bridge /lidar@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan"
+
+process2 = subprocess.Popen(
+    "gnome-terminal -e 'bash -c \""+My_Cmmnd3+";bash\"'",
     stdout=subprocess.PIPE,
     stderr=None,
     shell=True
