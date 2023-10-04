017845e5b41197ad32c54cb439c875454d45d05d
Jack2153 <55502158+Jack2153@users.noreply.github.com>
2023-10-04 23:29:47 +0200

    Add files via upload

--- startupSimSITL.py
+++ startupSimSITL.py
@@ -1,41 +1,57 @@
+#!usr/bin/env python3
 #Importing the required libraries
 import subprocess
 import time
 
-#Using the system.Popen() method to execute shell commands
+ #Using the system.Popen() method to execute shell commands
 
 print("Starting the Simulator...")
-
-My_Cmmnd1 = "gz sim -v 4 -r iris_empty.sdf"
-
-process2 = subprocess.Popen(
-    "gnome-terminal -e 'bash -c \""+My_Cmmnd1+";bash\"'",
-    stdout=subprocess.PIPE,
-    stderr=None,
-    shell=True
-)
-
+subprocess.Popen("gz sim -v 4 -r iris_empty.sdf", shell=True)
 #time.sleep() function delays the programm in seconds
 time.sleep(3)
-
 print("Starting the SITL...")
 print("This may take a few moments")
 
-My_Cmmnd2 = "sim_vehicle.py -v ArduCopter -f gazebo-iris --model JSON --map --console"
+My_Cmmnd = "sim_vehicle.py -v ArduCopter -f gazebo-iris --model JSON --map --console --debug"
 
-process2 = subprocess.Popen(
-    "gnome-terminal -e 'bash -c \""+My_Cmmnd2+";bash\"'",
+process = subprocess.Popen(
+    "gnome-terminal -e 'bash -c \""+My_Cmmnd+";bash\"'",
     stdout=subprocess.PIPE,
     stderr=None,
     shell=True
 )
 
-print("Starting the ROS_gz bridge)
+My_Cmmnd = " ros2 run ros_gz_bridge parameter_bridge /lidar_front@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan"
 
-My_Cmmnd3 = "ros2 run ros_gz_bridge parameter_bridge /lidar@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan"
+process = subprocess.Popen(
+    "gnome-terminal -e 'bash -c \""+My_Cmmnd+";bash\"'",
+    stdout=subprocess.PIPE,
+    stderr=None,
+    shell=True
+)
 
-process2 = subprocess.Popen(
-    "gnome-terminal -e 'bash -c \""+My_Cmmnd3+";bash\"'",
+My_Cmmnd = " ros2 run ros_gz_bridge parameter_bridge /lidar_back@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan"
+
+process = subprocess.Popen(
+    "gnome-terminal -e 'bash -c \""+My_Cmmnd+";bash\"'",
+    stdout=subprocess.PIPE,
+    stderr=None,
+    shell=True
+)
+
+My_Cmmnd = " ros2 run ros_gz_bridge parameter_bridge /lidar_right@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan"
+
+process = subprocess.Popen(
+    "gnome-terminal -e 'bash -c \""+My_Cmmnd+";bash\"'",
+    stdout=subprocess.PIPE,
+    stderr=None,
+    shell=True
+)
+
+My_Cmmnd = " ros2 run ros_gz_bridge parameter_bridge /lidar_left@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan"
+
+process = subprocess.Popen(
+    "gnome-terminal -e 'bash -c \""+My_Cmmnd+";bash\"'",
     stdout=subprocess.PIPE,
     stderr=None,
     shell=True
