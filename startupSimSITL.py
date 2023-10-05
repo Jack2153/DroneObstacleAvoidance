f579c74c7f5cd2c40194ce15cdbf7b568c79c4cc
Jack2153 <55502158+Jack2153@users.noreply.github.com>
2023-10-06 00:26:09 +0200

    Update startupSimSITL.py

--- startupSimSITL.py
+++ startupSimSITL.py
@@ -1,17 +1,18 @@
 #!usr/bin/env python3
-#Importing the required libraries
+# Importing the required libraries
 import subprocess
 import time
 
- #Using the system.Popen() method to execute shell commands
+# Using the system.Popen() method to execute shell commands
 
 print("Starting the Simulator...")
 subprocess.Popen("gz sim -v 4 -r iris_empty.sdf", shell=True)
-#time.sleep() function delays the programm in seconds
+# time.sleep() function delays the programm in seconds
 time.sleep(3)
 print("Starting the SITL...")
 print("This may take a few moments")
 
+# Launching SITL in a seperate shell
 My_Cmmnd = "sim_vehicle.py -v ArduCopter -f gazebo-iris --model JSON --map --console --debug"
 
 process = subprocess.Popen(
@@ -21,6 +22,7 @@ process = subprocess.Popen(
     shell=True
 )
 
+# Launching the ros_gz_bridge for the front sensor in a seperate shell
 My_Cmmnd = " ros2 run ros_gz_bridge parameter_bridge /lidar_front@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan"
 
 process = subprocess.Popen(
@@ -30,6 +32,7 @@ process = subprocess.Popen(
     shell=True
 )
 
+# Launching the ros_gz_bridge for the back sensor in a seperate shell
 My_Cmmnd = " ros2 run ros_gz_bridge parameter_bridge /lidar_back@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan"
 
 process = subprocess.Popen(
@@ -39,6 +42,7 @@ process = subprocess.Popen(
     shell=True
 )
 
+# Launching the ros_gz_bridge for the right sensor in a seperate shell
 My_Cmmnd = " ros2 run ros_gz_bridge parameter_bridge /lidar_right@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan"
 
 process = subprocess.Popen(
@@ -48,6 +52,7 @@ process = subprocess.Popen(
     shell=True
 )
 
+# Launching the ros_gz_bridge for the left sensor in a seperate shell
 My_Cmmnd = " ros2 run ros_gz_bridge parameter_bridge /lidar_left@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan"
 
 process = subprocess.Popen(
